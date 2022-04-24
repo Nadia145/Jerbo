fb1104b2a435c13feb9c0b92a8fd229aef182e02
YrandaNova <72846683+YrandaNova@users.noreply.github.com>
2022-04-24 18:14:28 -0500

    commit

--- Jerbo/Assets/Scripts/Inicio_Sesion/web.cs
+++ Jerbo/Assets/Scripts/Inicio_Sesion/web.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Globalization;
-using System;
 using UnityEngine.Networking;
 
-public class web : MonoBehaviour
+public class Web : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        // A correct website page.
+        //StartCoroutine(GetRequest("http://localhost/UnityBackendTutorial/GetData.php"));
+        //StartCoroutine(Login("Yranda","Nova"));
+        //StartCoroutine(RegisterUser("Vic","linux123"));
     }
 
-    public IEnumerator RegisterUser(string username, string password)
+    // Update is called once per frame
+    IEnumerator GetRequest(string uri)
     {
-        DateTime fecha = DateTime.Now;
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+
+    public IEnumerator RegisterUser(string username, string password, string date)
+    {
         WWWForm form = new WWWForm();
         form.AddField("loginUser", username);
         form.AddField("loginPass", password);
@@ -36,4 +61,5 @@ public class web : MonoBehaviour
             }
         }
     }
+
 }
