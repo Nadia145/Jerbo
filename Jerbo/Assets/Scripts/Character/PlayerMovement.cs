1ce751bc94a9fa14dc6b6c0c7ca296bcd765b6af
Nadia <nadiapaola145@gmail.com>
2022-05-01 17:49:10 -0500

    Modificación Script Personaje

--- Jerbo/Assets/Scripts/Character/PlayerMovement.cs
+++ Jerbo/Assets/Scripts/Character/PlayerMovement.cs
@@ -1,56 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*Nad*/
+/*Nad y Pablo*/
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Componentes")]
     private Rigidbody2D rb2D;
 
-    public float velocidadMovimiento;
-    public float fuerzaSalto;
-    private bool estaSaltando;
+    [Header("Layer Masks")]
+    private LayerMask capaTierra;
+
+    [Header("Variables Caminar")]
+    [SerializeField] private float movimientoAceleracion;
+    [SerializeField] private float maximaVelocidadMovimiento;
+    [SerializeField] private float linearDrag;
     private float movimientoHorizontal;
-    private float movimientoVertical;
-    void Start()
+    private bool cambiarDireccion => (rb2D.velocity.x > 0f && movimientoHorizontal < 0f) || (rb2D.velocity.x < 0f && movimientoHorizontal > 0f);
+
+    [Header("Variables Salto")]
+    [SerializeField] private float fuerzaSalto = 12f;
+    public float multiplicadorCaida = 2.5f;
+    public float multiplicadorSaltoBajo = 2f;
+
+    [Header("Variables Colisiones Tierra")]
+    [SerializeField] private float longitudRaycastTierra;
+    private bool tocandoTierra;
+
+    private void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-
-        estaSaltando = false;
     }
 
     void Update()
     {
-        movimientoHorizontal = Input.GetAxisRaw("Horizontal");
-        movimientoVertical = Input.GetAxisRaw("Jump"/*Omi esta linea*/);
+        movimientoHorizontal = ObtenerInput().x;
+        if (Input.GetButtonDown("Jump"/*Omi esta linea*/))
+            Saltando();
     }
 
+    //Aqui empiezan las funciones para el movimiento del personaje
     private void FixedUpdate()
     {
-        if (movimientoHorizontal > 0.1f || movimientoHorizontal < -0.1f) 
+        MovimientoPersonaje();
+        AplicarLinearDrag();
+    }
+
+    private Vector2 ObtenerInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void MovimientoPersonaje() 
+    {
+        rb2D.AddForce(new Vector2(movimientoHorizontal, 0f) * movimientoAceleracion);
+
+        if (Mathf.Abs(rb2D.velocity.x) > maximaVelocidadMovimiento) 
         {
-            rb2D.AddForce(new Vector2(movimientoHorizontal * velocidadMovimiento, 0f), ForceMode2D.Impulse);
-        }
-        if (!estaSaltando && movimientoVertical > 0.1f)
-        {
-            rb2D.AddForce(new Vector2(0f, movimientoVertical * fuerzaSalto), ForceMode2D.Impulse);
+            rb2D.velocity = new Vector2(Mathf.Sign(rb2D.velocity.x) * maximaVelocidadMovimiento, rb2D.velocity.y);
         }
     }
+
+    private void AplicarLinearDrag() 
+    {
+        if (Mathf.Abs(movimientoHorizontal) < 0.4f || cambiarDireccion)
+        {
+            rb2D.drag = linearDrag;
+        }
+        else
+        {
+            rb2D.drag = 0f;  
+        }
+    }
+    //Aqui terminan las funciones para el movimiento del personaje
+
+
+
+    //EAqui empiezan las funciones para el salto del personaje
+    private void Saltando()
+    {
+        rb2D.velocity = new Vector2(rb2D.velocity.x, 0f);
+        rb2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+    }
+
+    private void checarColisiones() 
+    {
+        tocandoTierra = Physics2D.Raycast(transform.position * longitudRaycastTierra, Vector2.down, longitudRaycastTierra, capaTierra);
+    }
+    //Aqui terminan las funciones para el salto del personaje
+
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Platform") 
-        {
-            estaSaltando = false;
-        }
         if (collision.gameObject.CompareTag("Headphones"))
         {
             Destroy(collision.gameObject);
         }
-    }
-
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        estaSaltando = true;
     }
 }
