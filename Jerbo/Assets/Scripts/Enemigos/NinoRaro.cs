b29876f0676ab0cf2450f66cc46a34f7be062c98
a01752030 <69481083+a01752030@users.noreply.github.com>
2022-05-02 14:39:15 -0500

    Commit

--- Jerbo/Assets/Scripts/Enemigos/NinoRaro.cs
+++ Jerbo/Assets/Scripts/Enemigos/NinoRaro.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class NinoRaro : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int health;
+    public float speed;
+
+    public GameObject BloodEffect;
+
+    public bool FindPlayer;
+
+    public Animator enemyAnim;
+
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void GiveDamage()
+    {
+        if (FindPlayer)
+        {
+            //Damage
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.transform.name == "Player")//or tag
+            {
+                //biu
+            }
+
+        }
     }
 }
